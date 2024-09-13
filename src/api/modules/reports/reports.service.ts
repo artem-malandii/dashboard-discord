b1c74bf3dbeb8dea44f732c9622045f744ea4936
Artem Malandii <artem.mal@code-care.pro>
2024-09-13 16:21:12 +0200

    reports: return array of reports

--- src/api/modules/reports/reports.service.ts
+++ src/api/modules/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { firstValueFrom } from 'rxjs';
 import { ReportEntity } from '../../../discord/entities/report.entity';
+import { APIUser } from 'discord-api-types/v10';
 
 @Injectable()
 export class ReportsService {
@@ -18,21 +19,46 @@ export class ReportsService {
   async getReports() {
     try {
       const reports = await this.reportsRepository.find();
-      return reports;
+
+      const modifiedReports = await Promise.all(
+        reports.map(async (report) => {
+          const user = await this.getUserInfo(report.reporterId);
+          let avatarLink = '';
+
+          if (user.avatar) {
+            const avatarFormat = user.avatar.startsWith('a_') ? 'gif' : 'png';
+            avatarLink = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${avatarFormat}`;
+          } else {
+            avatarLink = `https://cdn.discordapp.com/embed/avatars/${parseInt(user.discriminator) % 5}.png`;
+          }
+
+          return {
+            id: report.id,
+            avatarLink: avatarLink,
+            reportLink: report.link,
+            project: report.project,
+            date: report.date,
+            report: report.report,
+            reporterName: user.global_name ? user.global_name : user.username,
+          };
+        }),
+      );
+
+      return modifiedReports;
     } catch (e) {
       console.log(e);
     }
   }
 
-  async getUserInfo(): Promise<any> {
-    const url = `${this.discordApiUrl}/users/1160824008116285521`;
+  private async getUserInfo(userDiscordId: string) {
+    const url = `${this.discordApiUrl}/users/${userDiscordId}`;
     const headers = {
       Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
     };
 
     try {
       const response = await firstValueFrom(
-        this.httpService.get(url, { headers }),
+        this.httpService.get<APIUser>(url, { headers }),
       );
       return response.data;
     } catch (e: any) {
